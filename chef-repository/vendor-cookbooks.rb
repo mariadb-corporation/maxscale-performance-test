e6646a7d9a98efbed63982c16632b0308ea9f61e
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2018-03-26 20:50:02 +0300

    Remove previous version of vendored cookbooks when rendering it (refs #23853)

--- chef-repository/vendor-cookbooks.rb
+++ chef-repository/vendor-cookbooks.rb
@@ -2,14 +2,18 @@
 
 # This script vendors all the cookbooks into a special directory
 
+require 'fileutils'
+
 def vendor_cookbooks
   root = File.absolute_path(File.dirname(__FILE__))
+  vendored = "#{root}/vendor-cookbooks"
+  FileUtils.rm_rf(vendored)
   cookbooks_path = "#{root}/cookbooks"
   Dir.foreach(cookbooks_path) do |cookbook|
     next if cookbook =~ /^\./
     puts "Vendoring '#{cookbook}' cookbook."
     Dir.chdir("#{cookbooks_path}/#{cookbook}")
-    puts `berks vendor #{root}/vendor-cookbooks`
+    puts `berks vendor #{vendored}`
   end
   Dir.chdir root
 end
