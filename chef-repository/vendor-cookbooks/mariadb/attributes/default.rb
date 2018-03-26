b74bcc28d44f12a70c6e69f46c2a3102a16c1857
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2018-03-26 14:39:14 +0300

    Update vendored cookbooks according to the changes in the mariadb recipe (refs #23853)

--- chef-repository/vendor-cookbooks/mariadb/attributes/default.rb
+++ chef-repository/vendor-cookbooks/mariadb/attributes/default.rb
@@ -1,11 +1,9 @@
 # attributes/default.rb
 
 # mariadb version
-default["mariadb"]["version"] = "10.0"
-#default["mariadb"]["version"] = [ "5.5", "10.0" ]
+default["mariadb"]["version"] = "10.2"
 
 # mariadb repo ubuntu/debian/mint
-#default["mariadb"]["repo"] = "http://mirror.mephi.ru/mariadb/repo"
 default["mariadb"]["repo"] = "http://mirror.netinch.com/pub/mariadb/repo/10.2/ubuntu/ xenial main"
 
 # mariadb repo key for rhel/fedora/centos/suse
@@ -13,4 +11,7 @@ default["mariadb"]["repo"] = "http://mirror.netinch.com/pub/mariadb/repo/10.2/ub
 default["mariadb"]["repo_key"] = " http://yum.mariadb.org/"
 
 # path for server.cnf file
-default["mariadb"]["cnf_template"] = " server1.cnf"
+default["mariadb"]["cnf_template"] = "server1.cnf"
+
+# number of multi servers to setup
+default['mariadb']['servers'] = 4
