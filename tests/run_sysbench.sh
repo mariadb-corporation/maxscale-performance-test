8a84847a0df2e4cb66a96cbd891cb2846efd97c7
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2018-10-30 11:06:08 +0300

    Correct usage of the variables in .sh sysbench script (refs #24460)

--- tests/run_sysbench.sh
+++ tests/run_sysbench.sh
@@ -3,7 +3,7 @@ sysbench  --test=/usr/share/sysbench/oltp_read_write.lua\
          --db-driver=mysql  --mysql-db=test --mysql-user=skysql --mysql-password=skysql \
          --mysql-port=4006 --mysql-host=127.0.0.1  prepare
 sysbench  --test=/usr/share/sysbench/oltp_read_write.lua\
-         --threads=<% sysbench_threads %> \
+         --threads=<%= sysbench_threads %> \
          --mysql-host=127.0.0.1 --mysql-port=4006 --mysql-user=skysql --mysql-password=skysql \
          --db-driver=mysql  --mysql-db=test   \
          --max-requests=0  --time=300 run
--- tests/sample-test.rb
+++ tests/sample-test.rb
@@ -2,7 +2,7 @@
 
 # This is the simple application that only prints the environment variables and exits
 
-puts "All known environment variables"
+puts 'All known environment variables'
 ENV.each do |key, value|
   puts "#{key} = #{value}"
 end
